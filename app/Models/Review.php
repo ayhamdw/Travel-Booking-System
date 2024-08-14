d9f0d98c22fb8cc7202520718c246245032ce18b
ayhamdw <s12029113@stu.najah.edu>
2024-08-14 17:16:45 +0300

    Create

---
+++ app/Models/Review.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'comment',
+        'rating',
+        'reviewable_type',
+        'reviewable_id'
+    ];
+
+    public function reviewable()
+    {
+        return $this->morphTo();
+    }
+}
+
