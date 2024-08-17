e5e3a0510356e77533769689e650f6c22e8cef04
ayhamdw <s12029113@stu.najah.edu>
2024-08-17 18:26:21 +0300

    First Commit

---
+++ app/Models/HotelsBooking.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HotelsBooking extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'hotel_id',
+        'review_id',
+        'start_date',
+        'end_date'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function hotel()
+    {
+        return $this->belongsTo(Hotel::class);
+    }
+
+    public function review()
+    {
+        return $this->morphOne(Review::class, 'reviewable');
+    }
+}
