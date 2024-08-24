f18613816a9d7f1162aaf15979482c956a64feb4
ayhamdw <s12029113@stu.najah.edu>
2024-08-25 00:05:51 +0300

    Add All Pages, not completed

--- frontend/src/app/top-nav/top-nav.component.ts
+++ frontend/src/app/top-nav/top-nav.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-top-nav',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterOutlet
+  ],
   templateUrl: './top-nav.component.html',
   styleUrl: '../../assets/css/style.css'
 })
